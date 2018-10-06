7e5fea6d8ea00dc61b471bc541438588d86f4c06
Jan Freyberg <jan.freyberg@gmail.com>
2018-10-06 20:37:34 +0100

    Refactor cell magic to produce pystan.Model

--- setup.py
+++ setup.py
@@ -2,36 +2,36 @@
 
 from setuptools import setup
 
-readme = open('README.md').read()
+readme = open("README.md").read()
 
 setup(
-    name='stanmagic',
-    version='0.0.2',
-    description='An extension for Jupyter that help to run Stan code in '
-                'your interactive session.',
+    name="stanmodelmagic",
+    version="0.0.2",
+    description=(
+        "An extension for Jupyter that help to run Stan code in "
+        "your interactive session."
+    ),
     long_description=readme,
-    author='Aravind S',
-    author_email='arvindxxxx@gmail.com',
-    url='https://github.com/arvinds-ds/stanmagic',
+    author="Aravind S",
+    author_email="arvindxxxx@gmail.com",
+    url="https://github.com/arvinds-ds/stanmagic",
     py_modules=(
-        '_stan_builtins',
-        'stan_lexer',
-        'stan_compiler_output',
-        'stanmagic'
+        "_stan_builtins",
+        "stan_lexer",
+        "stan_compiler_output",
+        "stanmagic",
     ),
-    install_requires=(
-        'ipython',
-    ),
+    install_requires=("ipython",),
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: IPython',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+        "Development Status :: 3 - Alpha",
+        "Framework :: IPython",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
